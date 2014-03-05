c707a3f2032cb16fd9efd096839e6b288a0b02c5
Prithviraj Billa <prithviraj.m.billa@gmail.com>
2014-03-05 20:33:01 +0530

    added api/

--- hackathon/views.py
+++ hackathon/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import render
+from django.template.loader import get_template
+from django.template import Context
+
+
+def index(request):
+	return render(request,"index.html")
